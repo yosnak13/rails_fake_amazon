ad18e5a9373661aff8625f0cbf3792bae7ef2126
yosnak13 <yosnak13@gmail.com>
2020-12-27 21:57:19 +0900

    create category model and migrate

---
+++ db/migrate/20201227125506_create_categories.rb
@@ -0,0 +1,9 @@
+class CreateCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :categories do |t|
+      t.string :major_category_name
+      t.string :name
+      t.text :description
+    end
+  end
+end
