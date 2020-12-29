1ca816c71b46c5fc4768be989950d50a4a2032d0
yosnak13 <yosnak13@gmail.com>
2020-12-29 20:24:19 +0900

    remake routes; using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/edit'
-  get 'products/update'
-  get 'products/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products
 end
