ace8657d4aa8dfdf9ed4a0624e55696ff33bb9d5
yosnak13 <yosnak13@gmail.com>
2020-12-29 20:18:43 +0900

    create Products model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
+  get 'products/new'
+  get 'products/create'
+  get 'products/edit'
+  get 'products/update'
+  get 'products/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
