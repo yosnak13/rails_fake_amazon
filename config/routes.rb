32a0d65fa70d0be694c85f29f708dc588cbe5e2b
yosnak13 <yosnak13@gmail.com>
2020-12-30 13:15:48 +0900

    remake routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations',
+    :unlocks => 'users/unlocks',
+  }
+
+  devise_scope :user do
+    root :to => "users/sessions#new"
+    get 'singup', :to => 'users/registrations#new'
+    get 'login', :to => 'users/sessions#new'
+    get 'logout', :to => 'users/sessions#destroy'
+  end
   resources :products
 end
