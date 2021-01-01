f7ab6fa8ddacb93a6331f35c71dee75ae61bfbaa
yosnak13 <yosnak13@gmail.com>
2021-01-01 22:09:08 +0900

    change path after sign_in and sign_out

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,6 +18,14 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  def after_sign_in_path_for(user)
+    products_path
+  end
+
+  def after_sign_out_path_for(user)
+    root_path
+  end
+
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
