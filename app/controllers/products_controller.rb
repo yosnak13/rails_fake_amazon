0d2a6fe71fc6c42281a257ce7ecfcb0ad5cbecd7
yosnak13 <yosnak13@gmail.com>
2021-01-02 23:21:47 +0900

    action to add products as a favorite

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :create, :edit, :update, :destroy]
+  before_action :set_product, only: [:show, :create, :edit, :update, :destroy, :favorite]
 
   def index
     @products = Product.all
@@ -33,6 +33,11 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy
     redirect_to products_url
+  end
+
+  def favorite
+    current_user.toggle_like!(product)
+    redirect_to products_url product
   end
 
  private
