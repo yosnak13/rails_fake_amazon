27cfef1a0da1008c5606d3206bbe39e53a7a70f0
yosnak13 <yosnak13@gmail.com>
2021-01-04 00:10:27 +0900

    edit design of refine serch

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.display_list(category_params, params[:page])
+    @category = Category.request_category(category_params)
     @categories = Category.all
     @major_category_names = Category.major_categories
   end
