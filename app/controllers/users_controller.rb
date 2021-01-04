a208a9a02844f048162b147fd3a29bf61283d6f4
yosnak13 <yosnak13@gmail.com>
2021-01-04 16:36:40 +0900

    edit controller for user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
   def edit
   end
 
   def update
+    # update_without_passwordはgem deviseで使えるメソッドで定義不要。パスワードなしでユーザー情報を更新するメソッド。
+    @user.update_without_password(user_params)
+    redirect_to mypage_users_url
   end
 
   def mypage
   end
+
+  def edit_address
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.permit(:name, :email, :phone, :password_confirmation)
 end
