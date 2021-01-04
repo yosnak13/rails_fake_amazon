7a9b433d000b4eeb938ae07934dad01ac84a96b7
yosnak13 <yosnak13@gmail.com>
2021-01-04 11:48:34 +0900

    rails g controller users edit update mypage

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def edit
+  end
+
+  def update
+  end
+
+  def mypage
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/update'
+  get 'users/mypage'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
