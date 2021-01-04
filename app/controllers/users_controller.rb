79c13304fa2a89e8449d51d9a94ce5db07e27456
yosnak13 <yosnak13@gmail.com>
2021-01-04 23:40:09 +0900

    define update_passsword in controller and model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,20 @@ class UsersController < ApplicationController
   def edit_address
   end
 
+  def update_password
+    if password_set?
+      @user.update_password(user_params)
+      flash[:notice] = "パスワードは正しく更新されました。"
+      redirect_to root_url
+    else
+      @user.errors.add(:password, "パスワードに不備があります。")
+      render 'edit_password'
+    end
+  end
+
+  def edit_password
+  end
+
   private
 
   def set_user
@@ -25,4 +39,8 @@ class UsersController < ApplicationController
   def user_params
     params.permit(:name, :email, :address, :phone, :password_confirmation)
   end
+
+  def password_set?
+    user.params[:password].present? && user_params[:password_confirmation].present? ?
+    true : false
 end
