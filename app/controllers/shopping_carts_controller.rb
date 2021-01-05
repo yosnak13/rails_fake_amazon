af0731298ffcff9af321dffc9663eeb2a8f8fd36
yosnak13 <yosnak13@gmail.com>
2021-01-05 23:43:08 +0900

    user_cart_item method and user_cart_item_ids method

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -1,4 +1,16 @@
 class ShoppingCartsController < ApplicationController
+  before_action :set_cart, only: %i[index create destroy]
+
   def index
+    @user_cart_items = ShoppingCartItem.user_cart_items(@user_cart)
+    @user_cart_items_count = ShoppingCartItem.user_cart_items(@user_cart).count
+    @user_cart_item_ids = ShoppingCartItem.user_cart_item_ids(@user_cart)
+    @product_names = Product.in_cart_product_names(@user_cart_item_ids)
+    @total = @user_cart.total
+  end
+
+private
+  def set_cart
+    @user_cart = ShoppingCart.set_user_cart(current_user)
   end
 end
