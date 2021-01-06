5e5a8f8e8fcacedcf0bd5775f66d65e875d8f89b
yosnak13 <yosnak13@gmail.com>
2021-01-06 21:02:50 +0900

    create action

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -13,7 +13,17 @@ class ShoppingCartsController < ApplicationController
     @cart = ShoppingCart.find(user_id: current_user)
   end
 
+  def create
+    @product = Product.find(product_params[:product_id])
+    @user_cart.add(@product, product_params[:price].to_i, product_params[:quantity].to_i)
+    redirect_to product_url(@product)
+  end
+
 private
+  def product_params
+    params.permit(:product_id, :product, :price, :quantity)
+  end
+  
   def set_cart
     @user_cart = ShoppingCart.set_user_cart(current_user)
   end
