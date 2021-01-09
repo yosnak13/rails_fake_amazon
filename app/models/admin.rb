3494a4924a7b54b79713f01c8f2d47e2bcf492ac
yosnak13 <yosnak13@gmail.com>
2021-01-10 00:05:07 +0900

    define  limit of devise

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,8 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :rememberable, :validatable
+  #ログインとバリデーション機能のみ有効化
 end
