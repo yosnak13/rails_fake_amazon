b6c41433f9cdf2ca38ba3bea9982b461fba38c46
yosnak13 <yosnak13@gmail.com>
2021-01-09 21:58:52 +0900

    fix to that user can post review

--- app/models/product.rb
+++ app/models/product.rb
@@ -32,9 +32,10 @@ class Product < ApplicationRecord
 
   scope :in_cart_product_names, -> (cart_item_ids) { where(id: cart_item_ids).pluck(:name) }
 
-  # def reviews_new
-  #   reviews.new
-  # end
+  def reviews_new
+    reviews.new
+  end
+  
   def reviews_with_id
     reviews.reviews_with_id
   end
