158748fe5ba9329dd3050c67d954c5c1894f9782
yosnak13 <yosnak13@gmail.com>
2021-01-10 14:31:47 +0900

    edit path after redirect of log-in of admin

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -18,6 +18,14 @@ class Admins::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  def after_sign_in_path_for(user)
+    dashboard_path
+  end
+
+  def after_sign_out_path_for(user)
+    root_path
+  end
+
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
