888b4877609edfcd63dc7bf52c1e0562aae572c1
yosnak13 <yosnak13@gmail.com>
2021-01-10 16:38:24 +0900

    difine depended relation of table

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  has_many :products
+  has_many :products, dependent: :destroy
 
   extend DisplayList
   scope :major_categories, -> { pluck(:major_category_name).uniq }
