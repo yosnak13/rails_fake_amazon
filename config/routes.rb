0d8f84797966efd4504107e2e05675a4058e073a
yosnak13 <yosnak13@gmail.com>
2021-01-16 17:38:15 +0900

    edit route to manage major categories

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   end
 
   namespace :dashboard do
+    resources :major_categories, except: [:new]
     resources :categories, except: [:new]
     resources :products, except: [:show]
   end
