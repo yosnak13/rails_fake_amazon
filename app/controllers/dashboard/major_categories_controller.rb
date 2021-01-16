20d282cbb8f80623c8c8d8f35545fd2ce0bbb4fb
yosnak13 <yosnak13@gmail.com>
2021-01-16 17:55:54 +0900

    create controller for major category

---
+++ app/controllers/dashboard/major_categories_controller.rb
@@ -0,0 +1,31 @@
+class Dashboard::MajorCategoriesController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :set_major_category, only %w[show edit update destroy]
+  layout "dashboard/dashboard"
+
+  def index
+    @major_categories = Major_category.display_list(params[:pages])
+  end
+
+  def show
+  end
+
+  def create
+    major_category = MajorCategory.new(major_category_params)
+    major_category.save
+    redirect_to dashboard_major_categories_path
+  end
+
+  def edit
+  end
+
+  def destroy
+    @major_category.destroy
+    redirect_to dashboard_major_categories_path
+  end
+
+private
+  def major_category_params
+    params.require(:major_category).permit(:name, :description)
+  end
+end
