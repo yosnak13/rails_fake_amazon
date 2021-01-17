e6d4149e5a86ad53fdd32a56c322600baa95a82e
yosnak13 <yosnak13@gmail.com>
2021-01-17 18:51:10 +0900

    fix syntax product_params

--- app/controllers/dashboard/products_controller.rb
+++ app/controllers/dashboard/products_controller.rb
@@ -57,6 +57,7 @@ class Dashboard::ProductsController < ApplicationController
     end
 
     def product_params
-      params.require(:product).permit(:name, :description, :price, :category_id)
+      # params.require(:product).permit(:name, :description, :price, :category_id)
+      params.permit(:name, :description, :price, :category_id)
     end
 end
