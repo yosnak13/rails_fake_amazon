360f36b8822c80840949b61680c414ec0e433c50
yosnak13 <yosnak13@gmail.com>
2021-01-23 20:27:16 +0900

    define serch_information and display_list

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ApplicationRecord
   has_many :reviews
+  extend display_list
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
   acts_as_liker
+
+  scope :search_information, -> (keyword) {
+    where("name like ?","%#{keyword}%").
+    or(where("email like ?","%#{keyword}%")).
+    or(where("address like ?","%#{keyword}%")).
+    or(where("postal_code like ?","%#{keyword}%")).
+    or(where("phone like ?","%#{keyword}%")).
+    or(where("id ?","%#{keyword}%"))
+  }
 
   def update_password(params, *options)
     if params[:password].blank?
