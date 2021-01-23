e95ca550831901045e290c183ff47de108e796bb
yosnak13 <yosnak13@gmail.com>
2021-01-23 21:02:42 +0900

    define SwitchFlg module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :reviews
   extend DisplayList
+  extend SwitchFlg
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
