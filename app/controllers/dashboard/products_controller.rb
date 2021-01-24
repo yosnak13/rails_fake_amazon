38b5bc8c81c0d0b68f4040301ba0678ef1a3198b
yosnak13 <yosnak13@gmail.com>
2021-01-24 14:09:57 +0900

    edit CRUD

--- app/controllers/dashboard/products_controller.rb
+++ app/controllers/dashboard/products_controller.rb
@@ -58,6 +58,6 @@ class Dashboard::ProductsController < ApplicationController
 
     def product_params
       # params.require(:product).permit(:name, :description, :price, :category_id)
-      params.permit(:name, :description, :price, :category_id)
+      params.permit(:name, :description, :price, :recommend_flag, :category_id)
     end
 end
