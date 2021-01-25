43c0128fd70126d9d8defe45cf908aabdb9d4170
yosnak13 <yosnak13@gmail.com>
2021-01-25 20:30:52 +0900

    define scope 'bought_items' and 'order_updated_at_desc'

--- app/models/shopping_cart_item.rb
+++ app/models/shopping_cart_item.rb
@@ -1,5 +1,7 @@
 class ShoppingCartItem < ApplicationRecord
   acts_as_shopping_cart_item
   scope :user_cart_items, -> (user_shoppingcart) { where(owner_id: user_shoppingcart) }
+  scope :bought_items, -> (item_ids) { where(item_id: item_ids) }
+  scope :order_updated_at_desc, -> { order(updated_at: :desc) }
   scope :user_cart_item_ids, -> (user_shoppingcart) { where(owner_id: user_shoppingcart).pluck(:item_id) }
 end
