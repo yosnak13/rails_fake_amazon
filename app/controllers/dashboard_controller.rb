322b5b0c9852ac14678abf308afac2d1566f63f2
yosnak13 <yosnak13@gmail.com>
2021-01-25 20:30:06 +0900

    edit index method of proseeds

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,17 @@ class DashboardController < ApplicationController
   layout 'dashboard/dashboard'
 
   def index
+    @page != nil ? @page
+                 : 1
+    @sort = params[:sort]
+
+    @billings = if params[:sort] == "month"
+      ShoppingCart.get_monthly_billings
+    else
+      ShoppingCart.get_daily_billings
+    end
+
+    @total = ShoppingCart.bought_cart_ids.count
+    @sort_list = ShoppingCart.sort_list
   end
 end
