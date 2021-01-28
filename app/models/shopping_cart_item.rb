6d24891f3a5c61e40b6338791606fe2d6a28bcbb
yosnak13 <yosnak13@gmail.com>
2021-01-28 15:10:04 +0900

    define keep_item_ides, the method for keep item in shopping cart

--- app/models/shopping_cart_item.rb
+++ app/models/shopping_cart_item.rb
@@ -4,4 +4,5 @@ class ShoppingCartItem < ApplicationRecord
   scope :bought_items, -> (item_ids) { where(item_id: item_ids) }
   scope :order_updated_at_desc, -> { order(updated_at: :desc) }
   scope :user_cart_item_ids, -> (user_shoppingcart) { where(owner_id: user_shoppingcart).pluck(:item_id) }
+  scope :keep_item_ids, -> (user_cart_items) { where(owner_id: user_cart_items) }
 end
