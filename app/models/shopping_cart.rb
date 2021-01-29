b1b86ac02feaa22ea8d3e5b2727525678090c44c
yosnak13 <yosnak13@gmail.com>
2021-01-29 16:42:13 +0900

    define get_orders method and bought_carts scope

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -5,6 +5,15 @@ class ShoppingCart < ApplicationRecord
                                user_cart.nil? ? ShoppingCart.create(user_id: user.id)
                                               : user_cart }
   scope :bought_cart_ids, -> { where(buy_flag: true).pluck(:id) }
+  scope :bought_carts, -> (ids) { where("id LIKE ?", "%#{ids}%") }
+  # scope :bought_cart_user_ids_list, -> { where(buy_flag: true).pluck(:id, :user_id) }
+
+   scope :sort_list, -> {
+    {
+      "月別" => "month",
+      "日別" => "day",
+    }
+  }
 
   CARRIAGE=800
   FREE_SHIPPING=0
@@ -58,12 +67,22 @@ class ShoppingCart < ApplicationRecord
     return hash
   end
 
-  scope :sort_list, -> {
-    {
-      "月別" => "month",
-      "日別" => "",
-    }
-  }
+  def self.get_orders(code = {})
+    code.present? ? bought_carts = bought_carts(code[:code])
+                  : bought_carts = ""
+    return if bought_carts.blank?
+    cart_users_list = bought_cart_user_ids_list
+    user_ids_and_names_hash - User.where(id: cart_users_list).pluck(:id, :name).to_h
+
+    hash = Hash.new { |h, k| h[k] = {}}
+
+    bought_carts.each do |bought_cart|
+      hash[bought_cart.id][:user_name] = user_ids_and_names_hash[bought_cart.user.id]
+      hash[bought_cart.id][:updated_at] = bought_cart.updated_at.to_datetime.strftime("%Y-%m-%d %H:%M:%S")
+      hash[bought_cart.id][:price_total] = bought_cart.total.franctional / 100
+    end
+    return hash
+  end
 
   def tax_pct
     0
