b91126f2ac35420c7d5ceffb252f779e54cccb96
yosnak13 <yosnak13@gmail.com>
2021-01-29 16:41:35 +0900

    edit orders_controller

--- app/controllers/dashboard/orders_controller.rb
+++ app/controllers/dashboard/orders_controller.rb
@@ -1,2 +1,15 @@
 class Dashboard::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+  layout "dashboard/dashboard"
+
+  def index
+    code = params[:code].present? ?
+                                  params[:code]
+                                  : ""
+    @orders = code.present?
+                          ShoppingCart.get_orders({code: code})
+                          : ShoppingCart.get_orders
+    @prders_array = Kaminari.paginate_array(@prders.to_a).page(params{:page}).per(15)
+    @total = @orders.count
+  end
 end
